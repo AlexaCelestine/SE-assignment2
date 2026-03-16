9993fe8eb1cc3283d36cbb6dbfe4538bdc712e23
AlexaCelestine <acelestine3@pvamu.edu>
2026-03-16 17:47:41 -0500

    Changed greeting in feature-2 for conflict simulation

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    cout << "Hello from MAIN branch!" << endl;
+    cout << "Hello from FEATURE 2 branch!" << endl;
     cout << "Hello, World!" << endl;
     cout << "Hi! Alexa" << endl;
     return 0;
